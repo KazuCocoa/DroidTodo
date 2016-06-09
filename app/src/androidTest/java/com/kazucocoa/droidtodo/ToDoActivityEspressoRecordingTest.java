e565f52ec0b73cd8ae0448f7b9a3e0708f81d760
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-06-09 19:49:42 +0900

    add recorded test

---
+++ app/src/androidTest/java/com/kazucocoa/droidtodo/ToDoActivityEspressoRecordingTest.java
@@ -0,0 +1,54 @@
+package com.kazucocoa.droidtodo;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class ToDoActivityEspressoRecordingTest {
+
+    @Rule
+    public ActivityTestRule<ToDoActivity> mActivityTestRule = new ActivityTestRule<>(ToDoActivity.class);
+
+    @Test
+    public void toDoActivityEspressoRecordingTest() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(android.R.id.button1), withText("OK"),
+                        withParent(allOf(withId(R.id.buttonPanel),
+                                withParent(withId(R.id.parentPanel)))),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.fab), isDisplayed()));
+        floatingActionButton.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.input), isDisplayed()));
+        appCompatEditText.perform(replaceText("Espresso"));
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(android.R.id.button1), withText("OK"),
+                        withParent(allOf(withId(R.id.buttonPanel),
+                                withParent(withId(R.id.parentPanel)))),
+                        isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.todo_text_view), withText("Espresso"), isDisplayed()));
+        textView.check(matches(withText("Espresso")));
+
+    }
+}
